9cd0b83e112f2c063e95facd163eeaa73ff05fe2
Joel Chelliah <joel.chelliah@bekk.no>
2013-07-23 08:19:31 +0200

    Updated web.js bugfix

--- web.js
+++ web.js
@@ -1,15 +1,13 @@
-var express = require('express');
 var fs = require('fs');
+var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(32);
   fs.readFile('index.html', function (err, data) {
     if (err) throw err;
-    buffer.write(data, "utf-8");
+    response.send(data.toString('utf-8'));
   });
-  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
